a41d15566fdc581720bc31d4562204a9b313f32b
Nikita <nshamporovofficial@gmail.com>
2024-03-25 02:30:54 +0700

    update

--- PG.py
+++ PG.py
@@ -4,12 +4,9 @@ import string
 import mysql.connector
 import tkinter.messagebox
 
-# TODO: make it that when you leave PGPage and come back the acc_name field is empty    <-DONE->
-# TODO: fix error with multiplying password list in ViewP   <-DONE->
-# TODO: organise the password list in ViewP     <-DONE->
+# TODO: create functions for deleting and updating passwords
 # TODO: create scrolling list   <-(needs some tweaking)->
-# TODO: create EditP page
-# TODO: create DeleteP page
+# TODO: create copy button for ViewP
 
 # TODO (Optional): create page where users can create their own database
 # TODO (Optional): think of how can create sign up page
@@ -109,6 +106,57 @@ def inserting_password(account_name, generated_password):
                 connection.close()
 
 
+# function to get passwords from the database
+def getting_passwords(ViewP_instance):
+
+    # getting cursor for mysql queries 
+    cursor = get_cursor()
+
+    # if statement to check if cursor is connected (if not that means the user didn't log in)
+    if cursor:
+        try:
+            # mysql query to select all passwords from database
+            cursor.execute("select * from GeneratedPasswords")
+            passwords_list = cursor.fetchall()
+
+            # for loop to clear the list after left the page
+            for label in ViewP_instance.password_labels:
+                label.destroy()
+            ViewP_instance.password_labels.clear()
+
+            # if statement to check if there are any passwords in the database
+            if passwords_list:
+
+                # loop to show passwords if there are any
+                for idx, password in enumerate(passwords_list):
+                    # displaying id
+                    id_label = tk.Label(ViewP_instance.passwords_frame, text = password[0])
+                    id_label.grid(row = idx, column =0, padx = 5, pady = 5)
+                    ViewP_instance.password_labels.append(id_label)
+
+                    # displaying acc name
+                    acc_name_label = tk.Label(ViewP_instance.passwords_frame, text = password[1])
+                    acc_name_label.grid(row = idx, column = 1, padx = 5, pady = 5)
+                    ViewP_instance.password_labels.append(acc_name_label)
+
+                    # displaying password
+                    password_label = tk.Label(ViewP_instance.passwords_frame, text = password[2])
+                    password_label.grid(row = idx, column = 2, padx = 5, pady = 5)
+                    ViewP_instance.password_labels.append(password_label)
+            else:
+                # label to show that the are no passwords in the database
+                no_passwords_label = tk.Label(ViewP_instance.passwords_frame, text = "No passwords were saved in the database.")
+                no_passwords_label.pack()
+                ViewP_instance.password_labels.append(no_passwords_label)
+
+        except mysql.connector.Error as err:
+            # error alert box
+            tk.messagebox.showerror("Error", f"Error: {err}")   
+    else:
+        # warning box to tell user to log in
+        tk.messagebox.showwarning("Warning!", "Please log in first.")
+
+
 # class that creates the base of the app
 class PasswordGenerator(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -154,7 +202,6 @@ class login(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        
         # title widgets
         title = tk.Label(self, text = "Please Log In.")
         title.grid(columnspan = 2)
@@ -205,13 +252,16 @@ class Main(tk.Frame):
         title.grid(row = 0)
     
         PasswordGeneratorPage = tk.Button(self, text="Generate Password", command=lambda: controller.show_frame(PGpage))
-        PasswordGeneratorPage.grid(row = 1, padx = 5, pady = 5)
+        PasswordGeneratorPage.grid(row = 1, pady =3)
 
         PasswordViewPage = tk.Button(self, text = "View Passwords", command=lambda: controller.show_frame(ViewP))
-        PasswordViewPage.grid(row = 2, padx = 5, pady = 5)
+        PasswordViewPage.grid(row = 2, pady =3)
 
         PasswordEditPage = tk.Button(self, text = "Edit Passwords", command = lambda:controller.show_frame(EditP))
-        PasswordEditPage.grid(row = 3, padx = 5, pady = 5)
+        PasswordEditPage.grid(row = 3, pady =3)
+
+        PasswordDeletePage = tk.Button(self, text = "Delete Passwords", command = lambda:controller.show_frame(DeleteP))
+        PasswordDeletePage.grid(row = 4, pady =3)
 
         self.grid_columnconfigure(0, weight=1)
 
@@ -268,11 +318,13 @@ class PGpage(tk.Frame):
             tk.messagebox.showwarning("Warning!", "Please enter account name.")
             return
         
+        # send information to inserting_password()
         sent = inserting_password(account_name, generated_password)
         if sent:
             self.account_field.delete(0, tk.END)
             self.controller.show_frame(Main)
     
+    # function for back_button
     def backToMain(self):
         self.account_field.delete(0, tk.END)
         self.controller.show_frame(Main)
@@ -320,55 +372,8 @@ class ViewP(tk.Frame):
             # Scroll up if possible
             self.canvas.yview_scroll(-1, "units")
 
-    # function to get passwords from the database
     def getting_passwords(self):
-
-        # getting cursor for mysql queries 
-        cursor = get_cursor()
-
-        # if statement to check if cursor is connected (if not that means the user didn't log in)
-        if cursor:
-            try:
-                # mysql query to select all passwords from database
-                cursor.execute("select * from GeneratedPasswords")
-                passwords_list = cursor.fetchall()
-
-                # for loop to clear the list after left the page
-                for label in self.password_labels:
-                    label.destroy()
-                self.password_labels.clear()
-
-                # if statement to check if there are any passwords in the database
-                if passwords_list:
-
-                    # loop to show passwords if there are any
-                    for idx, password in enumerate(passwords_list):
-                        # displaying id
-                        id_label = tk.Label(self.passwords_frame, text = password[0])
-                        id_label.grid(row = idx, column =0, padx = 5, pady = 5)
-                        self.password_labels.append(id_label)
-
-                        # displaying acc name
-                        acc_name_label = tk.Label(self.passwords_frame, text = password[1])
-                        acc_name_label.grid(row = idx, column = 1, padx = 5, pady = 5)
-                        self.password_labels.append(acc_name_label)
-
-                        # displaying password
-                        password_label = tk.Label(self.passwords_frame, text = password[2])
-                        password_label.grid(row = idx, column = 2, padx = 5, pady = 5)
-                        self.password_labels.append(password_label)
-                else:
-                    # label to show that the are no passwords in the database
-                    no_passwords_label = tk.Label(self.passwords_frame, text = "No passwords were saved in the database.")
-                    no_passwords_label.pack()
-                    self.password_labels.append(no_passwords_label)
-
-            except mysql.connector.Error as err:
-                # error alert box
-                tk.messagebox.showerror("Error", f"Error: {err}")   
-        else:
-            # warning box to tell user to log in
-            tk.messagebox.showwarning("Warning!", "Please log in first.")
+        getting_passwords(self)
 
 
 class EditP(tk.Frame):
@@ -382,30 +387,38 @@ class EditP(tk.Frame):
             self.password = generate_password()
             displaying_password.config(text = self.password)
         
-        # Labels and entry fields for password details
-        tk.Label(self, text="Account Name:").grid(row=0, column=1, padx = 1, pady = 3)
-        self.account_name_entry = tk.Entry(self)
-        self.account_name_entry.grid(row=0, column=2, padx = 1, pady = 3)
+        title_label = tk.Label(self, text = "Edit Passwords")
+        title_label.grid(row = 0, columnspan=4)
 
-        tk.Label(self, text="Password:").grid(row=1, column=0, padx = 1, pady = 3)
+        # account related widgets
+        acc_name = tk.Label(self, text="Account Name:")
+        acc_name.grid(row=1, column=1, padx = 1, pady = 3, sticky='es')
+
+        self.acc_name_field = tk.Entry(self)
+        self.acc_name_field.grid(row=1, column=2, padx = 1, pady = 3, sticky='ws')
+        
+        # password related widgets
+        password_label = tk.Label(self, text="Password:")
+        password_label.grid(row=2, column=0, padx = 1, pady = 3, sticky='es')
+
         self.password_entry = tk.Entry(self, show="*")
-        self.password_entry.grid(row=1, column=1, padx = 1, pady = 3)
+        self.password_entry.grid(row=2, column=1, padx = 1, pady = 3, sticky='ws')
 
         regenarate_button = tk.Button(self, text = "Re-Generate", command = regenarate_password)
-        regenarate_button.grid(row = 1, column = 2, padx = 1, pady = 3)
+        regenarate_button.grid(row = 2, column = 2, )
 
         displaying_password = tk.Label(self,  text = self.password)
-        displaying_password.grid(row = 1, column = 3, padx = 1, pady = 3)
+        displaying_password.grid(row = 2, column = 3, )
 
-        
+        copy_button = tk.Button(self, text = "Copy", command = "smth")  # update command
+        copy_button.grid(row = 2, column = 4, padx = 1, pady = 3, )
 
-        # Button to update password
+        # update and back buttons
         update_button = tk.Button(self, text="Update Password")     # updating command
-        update_button.grid(row=2, column=1, padx = 1, pady = 3)
+        update_button.grid(row=3, column=1, padx = 1, pady = 3)
 
-        # Button to go back to the main page
-        back_button = tk.Button(self, text="Back", command=lambda: controller.show_frame(Main))
-        back_button.grid(row=2, column=2, padx = 1, pady = 3)
+        back_button = tk.Button(self, text="Back", command= self.backToMain)
+        back_button.grid(row=3, column=2, padx = 7, pady = 3, sticky='es')
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -417,6 +430,10 @@ class EditP(tk.Frame):
 
 
 
+     # function for back_button
+    def backToMain(self):
+        self.acc_name_field.delete(0, tk.END)
+        self.controller.show_frame(Main)
 
 
 class DeleteP(tk.Frame):
@@ -424,6 +441,30 @@ class DeleteP(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        title_label = tk.Label(self, text = "Delete Passwords")
+        title_label.grid(row = 0, columnspan=2, pady = 10)
+
+        # acc_name widgets
+        acc_name_label = tk.Label(self, text = "Enter Account Name: ")
+        acc_name_label.grid(row = 1, sticky = "es")
+
+        self.acc_name_field = tk.Entry(self)
+        self.acc_name_field.grid(row = 1, column = 1, sticky = "ws")
+
+        # delete and back buttons
+        delete_button = tk.Button(self, text = "Delete", command = "smth")  #edit command
+        delete_button.grid(row = 2, sticky = "es",padx = 8, pady = 6 )
+
+        back_button = tk.Button(self, text = "Back", command = self.backToMain)  # edit command
+        back_button.grid(row = 2, column = 1, sticky = "ws", padx = 8, pady = 6)
+
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)
+
+    # function for back_button
+    def backToMain(self):
+        self.acc_name_field.delete(0, tk.END)
+        self.controller.show_frame(Main)
 
 
 
